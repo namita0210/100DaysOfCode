c8fe0aa455d6f4e6181f1587bd364c0b6adcc1b0
Namita <46557775+namita0210@users.noreply.github.com>
2024-12-11 05:05:12 +0000

    Random module and Not Logic Gates

--- task.py
+++ task.py
@@ -1,5 +1,28 @@
-print(True)
-print(False)
-print(123)
-print( 12 + 1)
-print(123_456_789)
+# # print(True)
+# # print(False)
+# # print(123)
+# # print( 12 + 1)
+# # print(123_456_789)
+
+# #print("The number of letters in your name  : " + len(int(input("Your Name: "))))
+
+# # print(False or True or False)
+
+# a = 5
+# b = 7
+# if a >= b and a != b:
+#     print("A")
+# elif not a >= b and a != b:
+#     print("B")
+# else:
+#     print("C")        
+
+import random
+
+heads_or_tails = random.randint(0,1)
+print(heads_or_tails)
+
+if heads_or_tails == 1:
+    print("Heads")
+else:
+    print("Tails")    
