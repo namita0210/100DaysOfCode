b47f188c89821cd314c8dfefb41073ed86b51dd2
Namita <46557775+namita0210@users.noreply.github.com>
2024-12-23 16:47:18 +0000

    caesar cipher decode

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,5 +1,12 @@
-def decode_the_cipher():
-    pass
+def decode_the_cipher(cipher , shift):
+    text = ""
+    for letter in cipher:
+        correct_pos = alphabet.index(letter) - shift
+        if (correct_pos < 0):
+            temp = correct_pos
+            correct_pos = 26 + temp 
+        text += alphabet[correct_pos]
+    return text
 
 def encode_the_text(org_text, shift):
     cipher_text = ""
@@ -10,17 +17,18 @@ def encode_the_text(org_text, shift):
         else:
             shifted_pos = shifted_pos - 26
             cipher_text += alphabet[shifted_pos]    
-    return print(f"-------------\nEncoded Text: {cipher_text}\n----------------")
+    return cipher_text
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
 
 direction = input("Enter the function that you want to perform: \n").lower()
 
-text = input(f"Enter the text that you want to {direction}\n")
-
 if direction == "encode":
+    text = input(f"Enter the text that you want to {direction}\n")
     shift = int(input("Enter the number of spaces by which you want to shift: \n"))
-    encode_the_text(text,shift)
+    print(f"--------------------\n{encode_the_text(text,shift)}\n-------------------------")
 else:
-    decode_the_cipher()    
+    cipher = input(f"Enter the cipher that you want to {direction}\n")
+    shift = int(input("Enter the number of spaces by which you want to shift: \n"))
+    print(f"----------------\n{decode_the_cipher(cipher, shift)}\n--------------------")    
 
