4b1eeca006832300bdfeffa928aaf36648dad56f
Namita <46557775+namita0210@users.noreply.github.com>
2025-01-13 04:58:28 +0000

    exceptions stuff

--- exceptions_exercise.py
+++ exceptions_exercise.py
@@ -2,7 +2,9 @@ import random
 
 try:
     user_age = int(input("Please enter your age: "))
-    print(f"User is: {user_age} years old.")
+    print(f"User is {user_age} years old.")
 except ValueError:
     print(f"You have entered an invalid age as response. Please enter a numerical response, like {random.randint(1,101)}")
 
+if user_age > 18:
+    print(f"You can drive at age {user_age}")
