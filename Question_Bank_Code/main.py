7882c93e757ac4e224f87bdf4cbfb22ff01d7499
Namita <46557775+namita0210@users.noreply.github.com>
2025-01-13 04:58:00 +0000

    classes-objects stuff

---
+++ Question_Bank_Code/main.py
@@ -0,0 +1,10 @@
+from data import question_bank
+
+class Question:
+    def __init__(self, text, ans):
+        self.text = text
+        self.ans = ans
+
+for i in range(len(question_bank)):
+    #Create question object 
+    Question()        
