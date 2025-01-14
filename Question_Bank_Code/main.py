0b011f3028daca18590122e3773f1e8faeed2441
Namita <46557775+namita0210@users.noreply.github.com>
2025-01-14 08:57:47 +0000

    Looped in dictionary

--- Question_Bank_Code/main.py
+++ Question_Bank_Code/main.py
@@ -5,6 +5,9 @@ class Question:
         self.text = text
         self.ans = ans
 
-for i in range(len(question_bank)):
-    #Create question object 
-    Question()        
+#Iterate over question_data
+for key in question_bank:
+    #Create question object from each entry in question_data
+    q_obj = Question(key, val=question_bank.get(key))        
+    #Append each question object to question_bank
+    question_bank.append(q_obj)
