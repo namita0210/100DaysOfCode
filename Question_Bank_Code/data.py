223d85b1a9f75b97f316e0f583eb1c7420bb4fa4
Namita <46557775+namita0210@users.noreply.github.com>
2025-01-15 05:24:48 +0000

    modified question data

--- Question_Bank_Code/data.py
+++ Question_Bank_Code/data.py
@@ -1,34 +1,34 @@
-question_bank = {
-    "What is your name?": "My name is Shreya.",
-    "How old are you?": "I am 25 years old.",
-    "Where are you from?": "I am from India.",
-    "What do you do for a living?": "I am a software developer.",
-    "What is your favorite color?": "My favorite color is blue.",
-    "What is your hobby?": "I enjoy reading books.",
-    "Do you like sports?": "Yes, I love playing badminton.",
-    "What languages do you speak?": "I speak English, Hindi, and Bengali.",
-    "Do you have pets?": "Yes, I have a dog.",
-    "What is your favorite food?": "I love Italian food, especially pizza.",
-    "What is your favorite movie?": "My favorite movie is Inception.",
-    "What is your favorite book?": "I love 'To Kill a Mockingbird'.",
-    "Where do you live?": "I live in Bangalore.",
-    "What is your favorite season?": "I enjoy the winter season the most.",
-    "What is your favorite music genre?": "I enjoy listening to pop music.",
-    "What is your favorite holiday destination?": "I love visiting the beaches of Goa.",
-    "Do you like cooking?": "Yes, I enjoy trying out new recipes.",
-    "What is your favorite sport?": "Badminton is my favorite sport.",
-    "Do you like to travel?": "Yes, I love exploring new places.",
-    "What is your favorite technology?": "I am fascinated by Artificial Intelligence.",
-    "What is your dream job?": "My dream job is to be a data scientist.",
-    "Are you a morning person or night owl?": "I am a morning person.",
-    "What kind of movies do you enjoy?": "I enjoy science fiction and thrillers.",
-    "What is your favorite drink?": "I love drinking coffee."
-}
-thisdict = dict(name = "John", age = 36, country = "Norway")
-print(thisdict)
-for key in thisdict:
-    print(key)
-    print(thisdict.get(key))
+question_data = [
+    {"text": "What is your name?", "ans": "My name is Shreya."},
+    {"text": "How old are you?", "ans": "I am 25 years old."},
+    {"text": "Where are you from?", "ans": "I am from India."},
+    {"text": "What do you do for a living?", "ans": "I am a software developer."},
+    {"text": "What is your favorite color?", "ans": "My favorite color is blue."},
+    {"text": "What is your hobby?", "ans": "I enjoy reading books."},
+    {"text": "Do you like sports?", "ans": "Yes, I love playing badminton."},
+    {"text": "What languages do you speak?", "ans": "I speak English, Hindi, and Bengali."},
+    {"text": "Do you have pets?", "ans": "Yes, I have a dog."},
+    {"text": "What is your favorite food?", "ans": "I love Italian food, especially pizza."},
+    {"text": "What is your favorite movie?", "ans": "My favorite movie is Inception."},
+    {"text": "What is your favorite book?", "ans": "I love 'To Kill a Mockingbird'."},
+    {"text": "Where do you live?", "ans": "I live in Bangalore."},
+    {"text": "What is your favorite season?", "ans": "I enjoy the winter season the most."},
+    {"text": "What is your favorite music genre?", "ans": "I enjoy listening to pop music."},
+    {"text": "What is your favorite holiday destination?", "ans": "I love visiting the beaches of Goa."},
+    {"text": "Do you like cooking?", "ans": "Yes, I enjoy trying out new recipes."},
+    {"text": "What is your favorite sport?", "ans": "Badminton is my favorite sport."},
+    {"text": "Do you like to travel?", "ans": "Yes, I love exploring new places."},
+    {"text": "What is your favorite technology?", "ans": "I am fascinated by Artificial Intelligence."},
+    {"text": "What is your dream job?", "ans": "My dream job is to be a data scientist."},
+    {"text": "Are you a morning person or night owl?", "ans": "I am a morning person."},
+    {"text": "What kind of movies do you enjoy?", "ans": "I enjoy science fiction and thrillers."},
+    {"text": "What is your favorite drink?", "ans": "I love drinking coffee."}
+]
+# thisdict = dict(name = "John", age = 36, country = "Norway")
+# print(thisdict)
+# for key in thisdict:
+#     print(key)
+#     print(thisdict.get(key))
 #X = thisdict.items()
 # print(X)    
 # thisdict = {
@@ -39,3 +39,4 @@ for key in thisdict:
 # print(thisdict)
 # thisdict.update({"year": 2020})
 # print(thisdict)
+
